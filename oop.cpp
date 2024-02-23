09b9d80233782598bde5803374a719bc3659cb4f
optik1 <bearden86@gmail.com>
2024-02-22 20:38:29 -0500

    completed exercise for obj oriented programming 2

--- oop.cpp
+++ oop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector> 
 using namespace std;
 
 //class is a template for creating objects, describes the structure
@@ -16,12 +17,15 @@ using namespace std;
 //polymorphism - ability to use a single interface with different implementations
 
 
-class Person { //class describes structure
-private:
+class Person { //class describes structure //parent class 
+private: //if you change access modifier to "protected" you can access private data from derived classes 
     string first;
     string last;
 
     public:
+    Person(string first, string last): first(first), last(last) {} //constructor allows pass in data
+    Person() = default; //allows creation of empty object
+
     void setFirstName(string first) { 
         this->first = first; 
     }
@@ -38,14 +42,42 @@ private:
     void printFullName() {
         cout << first << " " << last << endl;
     }
+    virtual void printInfo() { //virtual and override are associated, virtual allows derived class to override base class
+        cout << "first name: " << first << endl;
+        cout << "last name: " << last << endl;
+    }
+    static void printPeople(vector<Person*> people) {
+        for(auto person: people) {
+            person->printInfo();
+        }
+    }
+};
+
+class Employee: public Person {  //derived class
+    string department;
+    public:
+    Employee(string firstName, string lastName, string department): Person(firstName, lastName), department(department) {}
+    Employee() = default;
+    string getDepartment() { return department; }
+    void setDepartment(string department) {
+        this->department = department;
+    }
+    void printInfo() override {
+        cout << "name: " << getName() << endl; //if modifier = protected, cout << "first name: " << first << endl;
+        cout << "department: " << department << endl; //if modifier = protected, cout << "last name: " << last << endl;
+    }
 };
 
 
 int main() {
+    vector<Person*> people;
+
+    // Person p("Matt", "Bearden"); // to use constructor
     Person p; //object that is a specific instance of the Person class; this is "instantiating"
     p.setFirstName("Matt");
     p.setLastName("Bearden");
 
+    // Person p2("John", "Smith"); // to use a constructor 
     Person p2;
     p2.setFirstName("John");
     p2.setLastName("Smith");
@@ -53,5 +85,29 @@ int main() {
     p.printFullName();
     p2.printFullName();
     cout << p.getName() << endl;
+
+    Person p3; //to use a default constructor, not pass in any data  
+    p3.printFullName();
+
+    Employee e;
+    e.setFirstName("Jared"); //access 
+    e.setLastName("Bearden");
+    e.printFullName();
+
+    Employee e2("Derek", "Bearden", "Software");
+    e2.printFullName();
+    cout << e2.getDepartment() << endl;
+    e2.printInfo();
+
+    people.push_back(&p);
+    people.push_back(&e);
+
+    Person::printPeople(people);
+
+    for(auto person: people) {
+        person->printInfo();
+    }
+
     return 0;
 }
+    
